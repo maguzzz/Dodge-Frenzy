42fffec1f2482e7c0686c88c6562b4a5a1f7eb61
Maguzzz <markusm.touray@gmail.com>
2023-03-31 12:39:19 +0200

    Score Cound and font added

--- Dodge Frenzy/Dodge Frenzy.cpp
+++ Dodge Frenzy/Dodge Frenzy.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
-using std::cout;
-using std::cin;
-using std::endl;
-
+using namespace std;
 
 int main()
 {
+	//Enemy
+	float EnemyVelocityX = 4;
+	float EnemyVelocityY = 4;
+
+	//Player
+	float playerSpeed = 6;
+	//Score
+	int ScoreNum = 000000000;
+	int HighScoreNum = 000000000;
+
+
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Dodge Frenzy");
 	window.setFramerateLimit(60);
 
+	//Adding Font
+	sf::Font pixelFont;
+	pixelFont.loadFromFile("C:\\Users\\Markus\\Documents\\DePixelHalbfett.ttf");
+
+	//Adding Score Text
+	sf::Text Score;
+	Score.setFont(pixelFont);
+	Score.setString("Score: \n000000000");
+	Score.setFillColor(sf::Color::White);
+	Score.setPosition(20, 20);
+	Score.setScale(.6, .6);
+
+	//Adding High Score
+	sf::Text HighScore;
+	HighScore.setFont(pixelFont);
+	HighScore.setString("High Score:\n000000000");
+	HighScore.setFillColor(sf::Color::White);
+	HighScore.setPosition(1110,20);
+	HighScore.setScale(.6, .6);
 
 	//Declearing a Rectangle called rec
 	sf::RectangleShape rect;
@@ -20,17 +47,12 @@ int main()
 	rect.setSize(sf::Vector2f(100, 100));
 	rect.setPosition(rectanglePosition);
 
-	float xVelocity = 0;
-	float yVelocity = 0;
-
 
 	//Player
 	sf::RectangleShape player;
 	sf::Vector2f playerPosition(520, 260);
 	player.setSize(sf::Vector2f(50, 50));
 	player.setPosition(playerPosition);
-
-	float playerSpeed = 6;
 
 
 	while (window.isOpen())
@@ -49,32 +71,32 @@ int main()
 			//Squar movement
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
 				playerPosition.y -= playerSpeed;
-				//player.move(0.f, -10.f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 				playerPosition.x -= playerSpeed;
-				//player.move(-10.f, 0.f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
 				playerPosition.y += playerSpeed;
-				//player.move(0.f, 10.f);
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 				playerPosition.x += playerSpeed;
-				//player.move(10.f, 0.f);
 			}
-
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::V)) {
+				ScoreNum = ScoreNum + 10;
+				cout << "ScoreNumber +1" << endl;
+				cout << ScoreNum << endl;
+			}
 			rect.setPosition(rectanglePosition);
 		}
 
 		// Calculating Hitbox by Moveing Origin point of Cube
 		if (playerPosition.y + 50 <= rectanglePosition.y || playerPosition.y >= rectanglePosition.y + 100 /* Horizontal */||  playerPosition.x + 50 <= rectanglePosition.x || playerPosition.x >= rectanglePosition.x + 100) {
-			cout << "Alive" << endl;
+			//cout << "Alive" << endl;
 			player.setFillColor(sf::Color::Green);
 		}
 
 		else {
-			cout << "Dead" << endl;
+			//cout << "Dead" << endl;
 			player.setFillColor(sf::Color::Red);
 		}
 
@@ -84,16 +106,16 @@ int main()
 
 
 		//Bound Check Cube                                    -100 Is Padding
-		if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - 100) xVelocity *= -1;
-		if (rectanglePosition.y < 0 || rectanglePosition.y > 720 - 100) yVelocity *= -1;
+		if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - 100) EnemyVelocityX *= -1;
+		if (rectanglePosition.y < 0 || rectanglePosition.y > 720 - 100) EnemyVelocityY *= -1;
 
 		//Checking Position for Debugging
-		cout << "Rectangle | " << rectanglePosition.y << " " << rectanglePosition.x << endl;
-		cout << "Player    | " << playerPosition.y << " " << playerPosition.x << endl;
+		//cout << "Rectangle | " << rectanglePosition.y << " " << rectanglePosition.x << endl;
+		//cout << "Player    | " << playerPosition.y << " " << playerPosition.x << endl;
 
 		//physics
-		rectanglePosition.x += xVelocity;
-		rectanglePosition.y += yVelocity;
+		rectanglePosition.x += EnemyVelocityX;
+		rectanglePosition.y += EnemyVelocityY;
 		rect.setPosition(rectanglePosition);
 		player.setPosition(playerPosition);
 
@@ -101,6 +123,8 @@ int main()
 		window.clear();
 		window.draw(rect);
 		window.draw(player);
+		window.draw(Score);
+		window.draw(HighScore);
 		window.display();
 
 	}
