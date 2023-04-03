56bb30d2f838650d31fcdfbd4916d92de3b8a053
Maguzzz <markusm.touray@gmail.com>
2023-04-03 14:44:53 +0200

    Player Death and Points Working

--- Dodge Frenzy/Dodge Frenzy.cpp
+++ Dodge Frenzy/Dodge Frenzy.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
+#include<iostream>
 #include "SFML/Graphics.hpp"
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
 	//Enemy
-	float EnemyVelocityX = 4;
-	float EnemyVelocityY = 4;
+	int EnemyVelocityX = 4;
+	int EnemyVelocityY = 4.2;
 
 	//Player
-	float playerSpeed = 6;
+	int playerSpeed = 6;
+	bool playerStatus = true;
 	//Score
-	int ScoreNum = 000000000;
-	int HighScoreNum = 000000000;
+	int ScoreNum = 0;
+	int HighScoreNum = 0;
 
 
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Dodge Frenzy");
@@ -21,12 +23,12 @@ int main()
 
 	//Adding Font
 	sf::Font pixelFont;
-	pixelFont.loadFromFile("C:\\Users\\Markus\\Documents\\DePixelHalbfett.ttf");
+	pixelFont.loadFromFile("Fonts/DePixelHalbfett.ttf");
 
 	//Adding Score Text
 	sf::Text Score;
 	Score.setFont(pixelFont);
-	Score.setString("Score: \n000000000");
+	Score.setString("Score: \n" + to_string(ScoreNum));
 	Score.setFillColor(sf::Color::White);
 	Score.setPosition(20, 20);
 	Score.setScale(.6, .6);
@@ -34,9 +36,9 @@ int main()
 	//Adding High Score
 	sf::Text HighScore;
 	HighScore.setFont(pixelFont);
-	HighScore.setString("High Score:\n000000000");
+	HighScore.setString("High Score:\n" + to_string(HighScoreNum));
 	HighScore.setFillColor(sf::Color::White);
-	HighScore.setPosition(1110,20);
+	HighScore.setPosition(1110, 20);
 	HighScore.setScale(.6, .6);
 
 	//Declearing a Rectangle called rec
@@ -69,49 +71,39 @@ int main()
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
 
 			//Squar movement
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-				playerPosition.y -= playerSpeed;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-				playerPosition.x -= playerSpeed;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-				playerPosition.y += playerSpeed;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-				playerPosition.x += playerSpeed;
-			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::V)) {
-				ScoreNum = ScoreNum + 10;
-				cout << "ScoreNumber +1" << endl;
-				cout << ScoreNum << endl;
-			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) playerPosition.y -= playerSpeed;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) playerPosition.x -= playerSpeed;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) playerPosition.y += playerSpeed;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) playerPosition.x += playerSpeed;
+
 			rect.setPosition(rectanglePosition);
 		}
 
-		// Calculating Hitbox by Moveing Origin point of Cube
-		if (playerPosition.y + 50 <= rectanglePosition.y || playerPosition.y >= rectanglePosition.y + 100 /* Horizontal */||  playerPosition.x + 50 <= rectanglePosition.x || playerPosition.x >= rectanglePosition.x + 100) {
-			//cout << "Alive" << endl;
+		//Score
+		if (playerStatus == true) {
 			player.setFillColor(sf::Color::Green);
-		}
+			ScoreNum = ScoreNum + 1;
 
+			Score.setString("Score: \n" + to_string(ScoreNum));
+			HighScore.setString("High Score:\n" + to_string(HighScoreNum));
+
+			//Increasing High Score if its lower then the current score
+			if(ScoreNum >= HighScoreNum) HighScoreNum = ScoreNum + 1;
+		}
 		else {
-			//cout << "Dead" << endl;
 			player.setFillColor(sf::Color::Red);
+			playerSpeed = 0;
 		}
 
-		//player.setFillColor(sf::Color::Green);
-
-
-
+		// Calculating Hitbox by Moveing Origin point of Cube
+		if (!(playerPosition.y + 50 <= rectanglePosition.y || playerPosition.y >= rectanglePosition.y + 100 /* Horizontal */ || playerPosition.x + 50 <= rectanglePosition.x || playerPosition.x >= rectanglePosition.x + 100)) playerStatus = false;
 
 		//Bound Check Cube                                    -100 Is Padding
-		if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - 100) EnemyVelocityX *= -1;
-		if (rectanglePosition.y < 0 || rectanglePosition.y > 720 - 100) EnemyVelocityY *= -1;
+		if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - 100) EnemyVelocityX *= -1.2;
+		if (rectanglePosition.y < 0 || rectanglePosition.y > 720 - 100)  EnemyVelocityY *= -1.2;
 
-		//Checking Position for Debugging
-		//cout << "Rectangle | " << rectanglePosition.y << " " << rectanglePosition.x << endl;
-		//cout << "Player    | " << playerPosition.y << " " << playerPosition.x << endl;
+		//Bound Check Cube                                    -50 Is Padding
+		if (playerPosition.x < 0 || playerPosition.x > 1280 - 50 || playerPosition.y < 0 || playerPosition.y > 720 - 50) playerStatus = false;
 
 		//physics
 		rectanglePosition.x += EnemyVelocityX;
@@ -121,12 +113,11 @@ int main()
 
 		//Render
 		window.clear();
-		window.draw(rect);
 		window.draw(player);
+		window.draw(rect);
 		window.draw(Score);
 		window.draw(HighScore);
 		window.display();
-
 	}
 	return 0;
 }
